ac59be9b895c552c2edd8936ee01499cae13c09b
LLosTy <marcojurgenjuras@gmail.com>
2024-04-27 18:31:05 +0200

    Fetching and populating event info component

--- src/components/event-info.tsx
+++ src/components/event-info.tsx
@@ -1,17 +1,57 @@
 import {Button} from "@/components/ui/button.tsx";
+import axios from 'axios';
+import {AxiosError, AxiosResponse} from "axios";
+import {useEffect, useState} from "react";
+
+//TODO Move the API to .env
+
+// const endpointUrl = 'https://api.example.com/data';
+
+// Make the Axios GET request
+
+
 
 
 export const EventInfo = () => {
+
+    // const [eventId, setEventId] = useState<string>("")
+    //
+    // const handleSetEventId = (newEventId: string) =>{
+    //     setEventId(newEventId);
+    // }
+
+    const [namePub, setNamePub] = useState<string>("")
+
+    const handleSetNamePub = (newNamePub: string) => {
+        setNamePub(newNamePub)
+    }
+
+    const [description, setDescription] = useState<string>("")
+
+    const handleSetDescription = (newDescription: string) =>{
+        setDescription(newDescription)
+    }
+
+    useEffect(() => {
+        axios.get('https://nfctron-frontend-seating-case-study-2024.vercel.app/event')
+            .then((response: AxiosResponse) => {
+                // handleSetEventId(response.data.eventId)
+                handleSetNamePub(response.data.namePub)
+                handleSetDescription(response.data.description)
+            })
+            .catch((error: AxiosError) => {
+                console.error('Error fetching data:', error);
+            });}, []);
+
+
     return (
         <aside className="w-full max-w-sm bg-white rounded-md shadow-sm p-3 flex flex-col gap-2">
             {/* event header image placeholder */}
             <div className="bg-zinc-100 rounded-md h-32"/>
             {/* event name */}
-            <h1 className="text-xl text-zinc-900 font-semibold">[event-name]</h1>
+            <h1 className="text-xl text-zinc-900 font-semibold">{namePub}</h1>
             {/* event description */}
-            <p className="text-sm text-zinc-500">[event-description]: Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Aliquam aliquid asperiores beatae deserunt dicta dolorem eius eos fuga laborum nisi
-                officia pariatur quidem repellendus, reprehenderit sapiente, sed tenetur vel voluptatibus?</p>
+            <p className="text-sm text-zinc-500">{description}</p>
             {/* add to calendar button */}
             <Button variant="secondary" disabled>
                 Add to calendar
